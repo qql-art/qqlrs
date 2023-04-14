daf56aa99d3c91005cbbd3b0fd05c284429f3b15
Willow Chargin <wchargin@gmail.com>
2023-04-14 14:12:14 -0700

    bin: add simple tool to spin RNG
    For testing against the source of truth.

--- src/bin/qql-cli.rs
+++ src/bin/qql-cli.rs
@@ -1,3 +1,9 @@
+use qql::rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = Rng::from_seed(b"");
+    for _ in 0..10_000_000 {
+        rng.gauss(0.0, 1.0);
+    }
+    println!("{}", rng.gauss(0.0, 1.0));
 }

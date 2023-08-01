c6a0549aabc428882341c6f423928abbff4ea758
Willow Chargin <wchargin@gmail.com>
2023-08-01 16:52:40 -0700

    sectors: fix panic on some very negative points
    If `radius < 0.0` is small and `x_min == x_max` and `y_min == y_max`,
    then the iteration will be over a single sector (the ranges are
    inclusive). But if `radius < 0.0` is large such that `x_min == x_max`
    but `y_min > y_max`, then the slicing will panic. In that case, we now
    emulate the bare `for` loop from the original JavaScript implementation
    by not iterating at all. We use a little extra bookkeeping to
    deduplicate these calculations.
    
    Seeds fixed by this:
    
    0xe25ecd59478ce509232e37c2e0810fd4c655b8b4223cb2f742c6ffff10ea0917
    0xe25ecd59478ce509232e37c2e0810fd4c655b8b4343952599a83ffff10ea0917
    0xe25ecd59478ce509232e37c2e0810fd4c655b8b469e2b4dd0d07ffff10ea0917
    
    (Thanks, `@heeey`!)
    
    Seeds to cross-test here:
    
    0xe25ecd59478ce509232e37c2e0810fd4c655b8b486bf1e348806ffff10e2cd33
    
    (Should not be affected by this change, but can be broken with a naive
    patch that causes *all* negative-radius points to be skipped entirely.)

--- src/sectors.rs
+++ src/sectors.rs
@@ -1,3 +1,5 @@
+use std::ops::RangeInclusive;
+
 use crate::{
     config::Config,
     math::{dist, dist_lower_bound, dist_upper_bound},
@@ -51,8 +53,14 @@ impl Sectors {
     /// Tests whether the given collider can be included in this sector grid without any
     /// collisions, and adds it if it can. Returns whether the collider was added.
     pub fn test_and_add(&mut self, collider: Collider) -> bool {
+        let Some(affected) = self.affected(&collider) else {
+            // Colliders affecting no sectors never collide and don't need to be added to anything.
+            // We can't actually iterate over this because colliders with very negative radii can
+            // have `y_max > y_min` and thus panic in the slice dereference.
+            return true;
+        };
         let fast_collisions = self.fast_collisions;
-        for sector in self.affected(&collider) {
+        for sector in self.sectors(&affected) {
             for other in sector {
                 if collides(fast_collisions, &collider, other) {
                     return false;
@@ -60,23 +68,47 @@ impl Sectors {
             }
         }
         // OK: no collisions.
-        for sector in self.affected(&collider) {
+        for sector in self.sectors(&affected) {
             sector.push(collider);
         }
         true
     }
 
-    fn affected(&mut self, collider: &Collider) -> impl Iterator<Item = &mut Vec<Collider>> {
+    /// Precondition: `collider.radius` must be non-negative.
+    fn affected(&self, collider: &Collider) -> Option<Affected> {
         let x_min = self.ix.index(collider.position.0 - collider.radius);
         let x_max = self.ix.index(collider.position.0 + collider.radius);
+        if x_min > x_max {
+            return None;
+        }
 
         let y_min = self.iy.index(collider.position.1 - collider.radius);
         let y_max = self.iy.index(collider.position.1 + collider.radius);
+        if y_min > y_max {
+            return None;
+        }
 
+        Some(Affected {
+            xs: x_min..=x_max,
+            ys: y_min..=y_max,
+        })
+    }
+
+    fn sectors(&mut self, affected: &Affected) -> impl Iterator<Item = &mut Vec<Collider>> {
+        let (x_min, x_max) = (*affected.xs.start(), *affected.xs.end());
+        let (y_min, y_max) = (*affected.ys.start(), *affected.ys.end());
         self.sectors[x_min..=x_max]
             .iter_mut()
             .flat_map(move |row| row[y_min..=y_max].iter_mut())
     }
+}
+
+/// Invariant: only constructed with `xs` and `ys` as well-formed ranges (i.e., that won't panic on
+/// slicing).
+#[derive(Debug)]
+struct Affected {
+    xs: RangeInclusive<usize>,
+    ys: RangeInclusive<usize>,
 }
 
 fn collides(fast_collisions: bool, c1: &Collider, c2: &Collider) -> bool {
